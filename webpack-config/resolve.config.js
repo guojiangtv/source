7bf03b35a3ff7a22f25af60c6c43d527862db264
saysmy <793501972@qq.com>
2017-10-19 18:24:09 +0800

    support vue debug

--- webpack-config/resolve.config.js
+++ webpack-config/resolve.config.js
@@ -1,10 +1,12 @@
 var path = require('path')
 var baseEntryDir = '../static_guojiang_tv/src/mobile/v2/'
 
+const vue_source = process.env.NODE_ENV === 'production' ? path.resolve(__dirname, baseEntryDir + 'js/lib/vue.min.js') : path.resolve(__dirname, baseEntryDir + 'js/lib/vue.js')
+
 module.exports = {
   // 模块别名的配置，为了使用方便，一般来说所有模块都是要配置一下别名的
 	alias: {
-		'vue': path.resolve(__dirname, baseEntryDir + 'js/lib/vue.min.js'),
+		'vue': vue_source,
 		'axios': path.resolve(__dirname, baseEntryDir + 'js/lib/axios.min.js'),
 		'layer': path.resolve(__dirname, baseEntryDir + 'js/lib/layer.js'),
 		'rsa': path.resolve(__dirname, baseEntryDir + 'js/lib/rsa.js'),
